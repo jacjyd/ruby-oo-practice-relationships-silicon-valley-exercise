135a93de5cbcfb081c200c9f99d226f27cf2806e
Diana Liao <dijali@gmail.com>
2021-02-26 18:02:22 -0500

    Deliverables seem to work.

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,59 @@
 class Startup
 
+    attr_accessor :name, :domain
+    attr_reader :founder
+
+    @@all_startups = []
+
+    def initialize (startup_name, founder, domain)
+        @name = startup_name
+        @founder = founder
+        @domain = domain
+
+        @@all_startups << self
+    end
+
+    def pivot (new_domain, new_name)
+        @name = new_name
+        @domain = new_domain
+    end 
+
+    def sign_contract (vc, inv_type, investment_amt)
+        new_funding_round = FundingRound.new(self, vc, inv_type, investment_amt)
+    end 
+
+    def funding_rounds
+        FundingRound.all.select{|fr| fr.startup == self}
+    end
+
+    def num_funding_rounds
+        funding_rounds.count
+    end
+
+    def total_funds
+        funding_rounds.map{|fr| fr.investment}.sum
+    end
+
+    def investors
+        funding_rounds.map{|fr| fr.venture_capitalist}.uniq
+    end
+
+    def big_investors
+        investors.select{|investor| VentureCapitalist.tres_commas_club.include?(investor)}
+    end
+
+
+    def self.domains
+        all.map{|startup| startup.domain}.uniq
+    end
+
+    def self.find_by_founder (founder_name)
+        error_message = "There is no founder of that name in the system."
+        all.find(ifnone = error_message){|startup| startup.founder == founder_name}
+    end
+
+    def self.all
+        @@all_startups
+    end
+
 end
--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,41 @@
 class VentureCapitalist
 
+    attr_accessor :name, :total_worth
+
+    @@all_vcs = []
+
+    def initialize (vc_name, worth)
+        @name = vc_name
+        @total_worth = worth
+
+        @@all_vcs << self
+    end
+
+    def offer_contract (startup, inv_type, inv_amt)
+        FundingRound.new(startup, self, inv_type, inv_amt)
+    end
+
+    def funding_rounds
+        FundingRound.all.select{|fr| fr.venture_capitalist == self}
+    end
+
+    def portfolio
+        funding_rounds.map{|fr| fr.startup}.uniq
+    end
+
+    def biggest_investment
+        funding_rounds.max{|fr_a,fr_b| fr_a.investment <=> fr_b.investment}
+    end
+
+    def invested (domain_string)
+        funding_rounds.select{|fr| fr.startup.domain = domain_string}.map{|fr| fr.investment}.reduce(:+)
+    end
+
+    def self.tres_commas_club
+        all.select{|vc| vc.total_worth > 1000000000}
+    end
+
+    def self.all
+        @@all_vcs
+    end 
 end
